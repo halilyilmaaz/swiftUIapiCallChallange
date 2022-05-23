988459cecb8a2cdc53c96ccea0993fde246e8f82
halilyilmaaz <haliilylmaaz@gmail.com>
2022-05-23 09:57:32 +0300

    Initial Commit

---
+++ swiftUIapiCallChallange/swiftUIapiCallChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIapiCallChallangeApp.swift
+//  swiftUIapiCallChallange
+//
+//  Created by halil yılmaz on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIapiCallChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
